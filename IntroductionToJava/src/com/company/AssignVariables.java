838fbdae90225c62b3a9a76fef4a020e81f85906
Simeon <walkzi@yandex.com>
2016-04-03 18:11:15 +0300

    IntroductionToJava

---
+++ IntroductionToJava/src/com/company/AssignVariables.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class AssignVariables {
+
+    public static void main(String[] args) {
+        byte assingByteValue = 127;
+        short assingShortValue = 32767;
+        int assingIntValue = 2000000000;
+        long assingLongValue = 919827112351L;
+        char assingCharValue = 'c';
+        boolean assingBoolValue = false;
+        float assingFloatValue = 0.5f;
+        double assingDoubleValue = 0.1234567891011d;
+        String assingStringValue = "Palo Alto, CA";
+    }
+}
